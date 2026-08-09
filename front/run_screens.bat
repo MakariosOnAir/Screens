e3b556f4d2b7af817974bf625488b2b472c5fdfb
Andrew Nour <andrewnour9@gmail.com>
2026-08-09 21:58:08 +0300

    Fix CMD parser parenthesis bug using flat goto jumps in launchers

--- front/run_screens.bat
+++ front/run_screens.bat
@@ -7,13 +7,15 @@ start "" "https://st-philopateer-screens.fly.dev/screens"
 echo Preparing companion script...
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://st-philopateer-screens.fly.dev/companion.ps1' -OutFile '%TEMP%\companion.ps1'"
 
-if exist "%TEMP%\companion.ps1" (
-    echo Set WshShell = CreateObject("WScript.Shell") > "%TEMP%\launch.vbs"
-    echo WshShell.Run "powershell -ExecutionPolicy Bypass -File ""%TEMP%\companion.ps1""", 0, false >> "%TEMP%\launch.vbs"
-    wscript "%TEMP%\launch.vbs"
-    del "%TEMP%\launch.vbs"
-) else (
-    echo Error: Failed to download companion script. Please check your internet connection.
-    pause
-)
+if not exist "%TEMP%\companion.ps1" goto error
+
+echo Set WshShell = CreateObject("WScript.Shell") > "%TEMP%\launch.vbs"
+echo WshShell.Run "powershell -ExecutionPolicy Bypass -File ""%TEMP%\companion.ps1""", 0, false >> "%TEMP%\launch.vbs"
+wscript "%TEMP%\launch.vbs"
+del "%TEMP%\launch.vbs"
+exit
+
+:error
+echo Error: Failed to download companion script. Please check your internet connection.
+pause
 exit
